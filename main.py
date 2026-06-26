796778d6b6d2ff0f201e0ff77b8b66baba548ed6
Murilo Santos <murilo.sntn10@gmail.com>
2026-06-26 01:27:12 -0300

    Aula4 finished

--- main.py
+++ main.py
@@ -15,12 +15,20 @@ def main():
     def carregar_dados(empresas):
         texto_tickers = " ".join(empresas)
         dados_acao = yf.Tickers(texto_tickers)
-        cotacoes_acao = dados_acao.history(start="2025-01-01", end="2026-06-18" ) #"YYYY-MM-DD"
+        cotacoes_acao = dados_acao.history(start="2025-01-01", end="2026-06-20" ) #"YYYY-MM-DD"
         print(cotacoes_acao)
         cotacoes_acao = cotacoes_acao["Close"] #a função tickers já retorna um Dataframe
         return cotacoes_acao
+    
+    @st.cache_data
+    def carregar_tickers_acoes():
+        base_tickers = pd.read_csv("acoes_b3_top50.csv", sep=";")
+        tickers = list(base_tickers["Acao"])
+        tickers = [item + ".SA" for item in tickers]
+        return tickers
 
-    acoes = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "WEGE3.SA"]
+
+    acoes = carregar_tickers_acoes()
 
     dados = carregar_dados(acoes)
     print(dados)
@@ -37,6 +45,7 @@ def main():
 
     #Filtro de ações
     lista_acoes = st.sidebar.multiselect("Escolha as ações para visualizar", dados.columns)
+    print("Lista ações: \n")
     print(lista_acoes)
     if lista_acoes:
         dados = dados[lista_acoes]
@@ -47,25 +56,43 @@ def main():
     #filtro de Datas
     data_inicial = dados.index.min().to_pydatetime()
     data_final = dados.index.max().to_pydatetime()
-    intervalo_data = st.sidebar.slider("Selecione o período", min_value=data_inicial , max_value=data_final, value=(data_inicial, data_final), step=timedelta(days=1))
-    print(intervalo_data)
+    intervalo_data = st.sidebar.slider("Selecione o período", min_value=data_inicial , max_value=data_final, value=(data_inicial, data_final), step=timedelta(days=1), format="DD/MM/YYYY")
 
     dados = dados.loc[intervalo_data[0]: intervalo_data[1]]
 
     #Criar o gráfico
+   
 
-    st.line_chart(dados)
+    st.line_chart(dados)    
 
+    if lista_acoes.__len__()==0:
+        lista_acoes = list(dados.columns)
+    elif lista_acoes.__len__()==1:
+        print(dados)
+        dados = dados.rename(columns={"Close" : acao_unica})
+        print(dados)
+        
+    lista_com_performace = list()
+
+    for acao in lista_acoes:
+        performace_ativo = (dados[acao].iloc[-1] / dados[acao].iloc[0]) -1
+        if performace_ativo > 0:
+            lista_com_performace.append(f"{acao}: :green[{performace_ativo:.1%}]")
+        elif performace_ativo < 0:
+            lista_com_performace.append(f"{acao}: :red[{performace_ativo:.1%}]")
+        elif performace_ativo == 0:
+            lista_com_performace.append(f"{acao}: :red[{performace_ativo:.1%}]")
+        else:        
+            lista_com_performace.append(f"{acao}: :yellow[{"Não há dados para o periodo selecionado"}]")
     
-    
-    st.write("""
-    # Fim do app
-             """)
+    st.write(f'''
+        ### Performace dos Ativos
+        Essa foi a performace de cada ativo selecionado:
+        ''')
+    for performace in lista_com_performace:
+        st.write(performace)    
 
     # criar a interface
-
-
-
 
 if __name__ == "__main__":
     main()
